916fbdf2fc1097a95c884541fcb7c5a55d7903a8
durnezj <thibault.durnez@student.howest.be>
2017-10-10 09:12:13 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         MinervaWebUtility mw = new MinervaWebUtility();
-        mw.login("tmdurnez","jjdst9d6");
+        mw.login("YOUR_USERNAME","YOUR_PASSWORD");
     }
 }
